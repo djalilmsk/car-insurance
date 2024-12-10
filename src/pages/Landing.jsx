e08135b789c2f8570d93647b22ac21f06c95d501
Abd Eldjallil Meskali <155311349+djalilmsk@users.noreply.github.com>
2024-12-10 15:24:47 +0100

    adds

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -3,16 +3,14 @@ import { Hero, PricingCard } from '../components';
 import About from './About';
 import Services from './Services';
 
-
 const Landing = () => {
   return (
     <div className="">
       <Hero />
       <About />
       <Services />
-      <div className=" flex gap-5">
-
-      {/* <PricingCard title={'Basic'} status={'Free'} price={'200'} buttonText={'Join Now'} plans={['we have this', 'and this', 'also this']}  />
+      <div className="flex gap-5">
+        {/* <PricingCard title={'Basic'} status={'Free'} price={'200'} buttonText={'Join Now'} plans={['we have this', 'and this', 'also this']}  />
       <PricingCard title={'Basic'} status={'Free'} price={'200'} buttonText={'Join Now'} plans={['we have this', 'and this', 'also this','gikbi', 'nougu']}  /> */}
       </div>
     </div>
